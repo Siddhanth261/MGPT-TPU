f2ec7e1732c048711bbc0ac7bb65f7b031e699be
Siddhanth Pandey <siddhantpandey488@gmail.com>
2025-05-09 15:10:00 -0500

    core: finalize p_train_step

--- synthesis/synthesis_core/train_loop.py
+++ synthesis/synthesis_core/train_loop.py
@@ -10,5 +10,8 @@ class TrainConfig:
 def train(cfg, init_fn, loss_fn, batch_fn, rng):
     params = init_fn(rng)
     grad_fn = jax.value_and_grad(loss_fn)
+    loss = jax.lax.pmean(loss, axis_name="d")
+    grads = jax.lax.pmean(grads, axis_name="d")
+    p_train_step = jax.pmap(train_step, axis_name="d")
 
     return params
